d37ed6d4ea04bf2c67fa5fe5fae01b0802db007a
Salsa <>
2019-04-01 16:10:12 -0700

    added train form and submit button, enters info to current train schedule

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Train_Time
+# Train_Time schedule
+
+ * This is an unordered list of items on Train_Time:
+ * This is a schedule of trains that come to a station.
+ * Names of trains and their destination. 
+ * List of how frequently that trains comes by 
+ * Next arrival time for that train.
+ * Count down of how many minutes away from arriving at the station.
+
+- There is also a add a train form with name, destination, first time train arrives and how       frequently.
+- This form submits to the Current Train Schedule form and becomes part of the list.
+
+* ** Note: ** This project needs index.html file, traintime-data.js and firebase         traintime-de7f3 to work.
--- traintime-data.js
+++ traintime-data.js
@@ -7,34 +7,158 @@ var config = {
     messagingSenderId: "494179362764"
   };
   firebase.initializeApp(config);
-  
-  var trainData = firebase.database();
 
-  var trains = [
-      ["Trenton Express", "Trenton", 25, "05:35PM"],
-      ["Oregon Trail", "Salem, Oregon", 3600, "01:39PM"],
-      ["Midnight Carriage", "Philadelphia", 15, "05:35PM"],
-      ["Sing Sing Caravan", "Atlanta", 45, "05:53PM"],
-      ["Boston Bus", "Boston", 65, "05:50PM"],
-      ["California Caravan", "San Francisco", 6000, "01:25AM"],
-      ["Analben's Train", "Florida", 25, "05:28PM"]
-  ]
+  //var getTrainData = firebase.database().ref();
+  var ref = firebase.database().ref();
+  var trainData = ref.child("trains");
 
+/*hard coded for firebase data
+trainData.push({
+  "name": "Trenton Express",
+  "destination": "Trenton",
+  "frequency": "25",
+  "firstTrain": "05:35"
+});
 
-var newTrain = {
-  name: name,
-  destination: destination,
-  firstTrain: firstTrain,
-  frequency: frequency
-};
+trainData.push({
+  "name": "Oregon Trail",
+  "destination": "Salem, Oregon",
+  "frequency": "3600",
+  "firstTrain": "01:39"
+});
 
-  var i;
-  for (i = 0; i < trains.length; i++) { 
-    newTrain.name=Trains[i,0];
-    newTrain.destination=Trains[i,1];
-    newTrain.firstTrain=Trains[i,3];
-    newTrain.frequency=Trains[i,2];
+trainData.push({
+  "name": "Midnight Carriage",
+  "destination": "Philadelphia",
+  "frequency": "15",
+  "firstTrain": "05:35"
+});
 
-    trainData.ref().push(newTrain);
+trainData.push({
+  "name": "Sing Sing Caravan",
+  "destination": "Atlanta",
+  "frequency": "45",
+  "firstTrain": "05:53"
+});
+
+trainData.push({
+  "name": "Boston Bus",
+  "destination": "Boston",
+  "frequency": "65",
+  "firstTrain": "05:50"
+});
+
+trainData.push({
+  "name": "California Caravan",
+  "destination": "San Francisco",
+  "frequency": "6000",
+  "firstTrain": "01:25"
+});
+
+trainData.push({
+  "name": "Analben's Train",
+  "destination": "Florida",
+  "frequency": "25",
+  "firstTrain": "05:28"
+}); 
+*/
+//var ref = firebase.database().ref("trains");
+var ref = firebase.database().ref();
+var trainData = ref.child("trains");
+
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabbed values from text-boxes
+  trainName = $("#trainNameData").val().trim();
+  destination = $("#destinationData").val().trim();
+  firstTrain = $("#f-trainTimeData").val().trim();
+  frequency = $("#frequencyData").val();
+  //console.log(parseInt(frequecy));
+
+  NewTrainData = {
+    "name": trainName,
+    "destination": destination,
+    "firstTrain": firstTrain,
+    "frequency": frequency
+  // Code for "Setting values in the database"
   }
+  //validate data to database
+  //console.log(NewTrainData);
+
+  //sending data to database
+  trainData.push(NewTrainData);
+
+  //empty train schedule and blank input fields
+  $("table tbody").empty(); 
+  $("#trainNameData").val("");
+  $("#destinationData").val("");
+  $("#f-trainTimeData").val("");
+  $("#frequencyData").val("");
+});
+
+//event for changes to database. updates train schedule.
+ref.on("value", function(snapshot) {
+  //data in database put in console for review. I want to see the data. before display worked.
+  console.log(snapshot.val());
+  
+  //getting info from trains collections
+  var getTrainData = firebase.database().ref("trains");
+  getTrainData.once("value")
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+
+        //taking data from database and breaking into fields for train schedule
+        var childData = childSnapshot.val();
+        var tabName = childData.name;
+        var tabDestination= childData.destination;
+        var tabFrequency= childData.frequency;
+        var tabFirstTrain= childData.firstTrain;
+
+        //just checking the data parsed into table.
+        console.log(tabName + ' - ' + tabDestination + ' - ' + tabFrequency + ' - ' + tabFirstTrain);
+
+        //moment.js calculations - assume start date 1 week ago and trains never stopped.
+        var startDate = "2019-03-25 " + tabFirstTrain;
+        var startDateTime = moment(startDate);
+        var minutesFromStart = startDateTime.diff(moment(), "minutes");
+        //console.log(minutesFromStart + " - "+ tabFrequency);
+        var minutesfromTrain = (-1 * minutesFromStart) % tabFrequency;
+        var minutesNextTrain = tabFrequency - minutesfromTrain
+        
+        var nextTrain = moment().add(minutesNextTrain, 'minutes').format('hh:mm A');
+
+        //created string for train schedule to the html
+        var trainTable =  "<tr><td>" + tabName + "</td><td>" + tabDestination +
+        "</td><td>" + tabFrequency +  "</td><td>" + nextTrain +  "</td><td>" + minutesNextTrain + "</td></tr>"
+         //adds train schedule to the html using string above
+        $("table tbody").append(trainTable); 
+
+     });
+  });
+
+});
+
+
+/* pseudo code
+three sections (HTML)
+  1. jumbotron
+  2. current train schedule (table)
+  3. add new train to schedule
+get 7 listed trains into database
+  1. write data to firebase using javascript
+  2. read data to firebase
+  3. need to separate each train data info from large database
+  4. add trains to html 
+add new train to database
+  4 fields - static data must have
+  submit button
+  clear old train data when update new information.
+make current time active- clock
+  military time 24 hours
+calculate next arrival based on 3 factors
+  1. what time is it?
+  2. first train time arrival
+  3. calculate how much time left before train arrives at station
+*/
 
